4db1113fd9cce3120f0baa1c3d208ce6b6874e36
HARDPC <rumpelpawel@gmail.com>
2024-06-04 23:28:47 +0200

    Meat now restores food.

--- src/main/java/net/cebularz/droppedbuffs/entity/custom/Meat_Buff_Entity.java
+++ src/main/java/net/cebularz/droppedbuffs/entity/custom/Meat_Buff_Entity.java
@@ -3,9 +3,11 @@ package net.cebularz.droppedbuffs.entity.custom;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
+import java.util.List;
 import java.util.Random;
 
 public class Meat_Buff_Entity extends Entity {
@@ -15,11 +17,14 @@ public class Meat_Buff_Entity extends Entity {
 
     public int age;
     public float rotationY;
+
+    public float alpha;
     public Meat_Buff_Entity(EntityType<?> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
         this.age=0;
         Random random = new Random();
         rotationY = random.nextFloat() * 360.0F;
+        alpha=1F;
     }
 
     @Override
@@ -29,6 +34,16 @@ public class Meat_Buff_Entity extends Entity {
 
     @Override
     public void tick() {
+        List<Entity> nearbyEntities = this.level().getEntitiesOfClass(Entity.class, this.getBoundingBox());
+        for (Entity entity : nearbyEntities) {
+            if (entity instanceof Player) {
+                int food = Math.min(20,((Player) entity).getFoodData().getFoodLevel()+8);
+                if(((Player) entity).getFoodData().getFoodLevel()<20) {
+                    ((Player) entity).getFoodData().setFoodLevel(food);
+                    this.discard();
+                    }
+                }
+            }
         super.tick();
         this.age++;
         if(this.age>=400){
@@ -36,7 +51,9 @@ public class Meat_Buff_Entity extends Entity {
         }
         rotationY += 3.0F;
 
-
+        if(age>=360){
+            alpha-=0.025f;
+        }
         // Keep values within 0-360 degrees
         rotationY %= 360.0F;
     }
