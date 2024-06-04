17246b6ab5b4f3c8277a135b30be18974cb3d430
HARDPC <rumpelpawel@gmail.com>
2024-06-04 11:13:58 +0200

    change

--- src/main/java/net/cebularz/droppedbuffs/entity/client/BuffRenderer.java
+++ src/main/java/net/cebularz/droppedbuffs/entity/client/BuffRenderer.java
@@ -1,25 +1,29 @@
 package net.cebularz.droppedbuffs.entity.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.cebularz.droppedbuffs.DroppedBuffs;
 import net.cebularz.droppedbuffs.entity.custom.BuffEntity;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class BuffRenderer extends EntityRenderer<BuffEntity> {
-    private final ModelPart bone;
+@OnlyIn(Dist.CLIENT)
+public class BuffRenderer extends MobRenderer<BuffEntity,Health_Drop<BuffEntity>> {
 
-    public BuffRenderer(EntityRendererProvider.Context pContext) {
-        super(pContext);
-        ModelPart bone = pContext.bakeLayer(ModModelLayers.BUFF_LAYER);
-        this.bone = bone;
-        this.shadowRadius = 0.5F;
+
+    public BuffRenderer(EntityRendererProvider.Context pContext, Health_Drop<BuffEntity> pModel, float pShadowRadius) {
+        super(pContext, new Health_Drop<>(pContext.bakeLayer(ModModelLayers.BUFF_LAYER)), 0.5f);
 
     }
+
 
     @Override
     public ResourceLocation getTextureLocation(BuffEntity buffEntity) {
@@ -29,5 +33,8 @@ public class BuffRenderer extends EntityRenderer<BuffEntity> {
     @Override
     public void render(BuffEntity pEntity, float pEntityYaw, float pPartialTicks, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight) {
         super.render(pEntity, pEntityYaw, pPartialTicks, pPoseStack, pBuffer, pPackedLight);
+
+
+
     }
 }
--- src/main/java/net/cebularz/droppedbuffs/entity/custom/BuffEntity.java
+++ src/main/java/net/cebularz/droppedbuffs/entity/custom/BuffEntity.java
@@ -3,12 +3,15 @@ package net.cebularz.droppedbuffs.entity.custom;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.level.Level;
 
-public class BuffEntity extends Entity {
-    public BuffEntity(EntityType<?> pEntityType, Level pLevel) {
+public class BuffEntity extends Mob {
+
+
+    public BuffEntity(EntityType<? extends Mob> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
@@ -17,14 +20,4 @@ public class BuffEntity extends Entity {
 
     }
 
-    @Override
-    protected void readAdditionalSaveData(CompoundTag compoundTag) {
-
-    }
-
-    @Override
-    protected void addAdditionalSaveData(CompoundTag compoundTag) {
-
-    }
-    
 }
